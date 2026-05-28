745e258688a40c3a10c1da481c0fbf12e9d3b573
armanssudrot-cmd <armans.sudrot@gmail.com>
2026-05-28 12:56:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
